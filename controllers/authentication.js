d51fe3f2ef53017972d00abd96797adb1e61b418
Andrew <andrew.vinocur@gmail.com>
2018-06-04 16:30:47 -0700

    sets up authentication controller

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,11 +1,28 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
+
   // see if a user w/ given email exists
-  
-  // if a user w/ email does exist, return an error
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
 
-  // if a user w/ new email, create and save user record
+    // if a user w/ email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
 
-  // respond to req indicating user was created
+    // if a user w/ new email, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // respond to req indicating user was created
+      res.json(user);
+    });
+  });
 }

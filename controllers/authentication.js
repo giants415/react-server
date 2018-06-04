db2e80da1161b694b606c13d2f30267a661f6105
Andrew <andrew.vinocur@gmail.com>
2018-06-04 16:18:03 -0700

    pulls of username and PW from req.body

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,11 @@
 exports.signup = function(req, res, next) {
-  res.send({ success: true });
+  const email = req.body.email;
+  const password = req.body.password;
+  // see if a user w/ given email exists
+  
+  // if a user w/ email does exist, return an error
+
+  // if a user w/ new email, create and save user record
+
+  // respond to req indicating user was created
 }

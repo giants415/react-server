1448644cfae664099f2120027106c42d13d7917f
Andrew <andrew.vinocur@gmail.com>
2018-06-04 10:30:47 -0700

    requires

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Main starting point of the app
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// App Set Up
+
+// Server Set Up

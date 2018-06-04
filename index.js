6326e07c6030a2f044cec52627df7602f9603af3
Andrew <andrew.vinocur@gmail.com>
2018-06-04 10:32:04 -0700

    creates instance of server w/ app

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const app = express();
 
 // App Set Up
 
 // Server Set Up
 const port = process.env.PORT || 3090;
+const server = http.createServer(app);

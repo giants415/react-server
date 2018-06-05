62482e75dc280cd970032db27e5ce6ebeecd8d5b
Andrew <andrew.vinocur@gmail.com>
2018-06-05 11:42:39 -0700

    notes

--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,10 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Set up options for JWT Strat
+const jwtOptions = {};
+
+// Create JWT strat
+
+// Tell passport to use this strat

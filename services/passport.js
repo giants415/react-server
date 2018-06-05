ae6cc14ef258f93e3b6d3b2166f58163fad3e000
Andrew <andrew.vinocur@gmail.com>
2018-06-05 11:50:04 -0700

    jwt strategy fully set up

--- services/passport.js
+++ services/passport.js
@@ -5,7 +5,10 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // Set up options for JWT Strat
-const jwtOptions = {};
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
 
 // Create JWT strat (payload = id and timestamp)
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
